4e24448966a16adba0fe5f50531840ce72d0e693
Khoa Huynh <khoawin92@gmail.com>
2018-05-16 00:55:37 -0700

    all tests running

---
+++ lab-khoa/test/greet.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const greet = require('../lib/greet');
+
+describe('greet module', () => {
+  it('should return null when passed non string', () => {
+    expect(greet()).toBeNull();
+  });
+
+  it('should return return "hello world" when passed world', () => {
+    expect(greet('world')).toBe('hello world');
+  });
+});
